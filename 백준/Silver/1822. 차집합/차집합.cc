8047ca4c0f3932acfc5ba2248045672830465444
starlike6617 <105418844+starlike6617@users.noreply.github.com>
2022-11-29 18:25:23 +0900

    [Silver IV] Title: 차집합, Time: 620 ms, Memory: 9020 KB -BaekjoonHub

---
+++ 백준/Silver/1822. 차집합/차집합.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int na, nb;
+    cin >> na >> nb;
+
+    vector<int> a(na), b(nb), res;
+    for (int i = 0; i < na; i++)
+        cin >> a[i];
+    for (int i = 0; i < nb; i++)
+        cin >> b[i];
+
+    sort(b.begin(), b.end());
+
+    for (int i = 0; i < a.size(); i++)
+        if (!binary_search(b.begin(), b.end(), a[i]))
+            res.push_back(a[i]);
+
+    sort(res.begin(), res.end());
+    cout << res.size() << '\n';
+    for (int i = 0; i < res.size(); i++)
+        cout << res[i] << ' ';
+}
