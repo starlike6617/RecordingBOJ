8869f722146a42e50a71065e809b433f33615d70
starlike6617 <105418844+starlike6617@users.noreply.github.com>
2022-12-05 19:46:57 +0900

    [Gold IV] Title: 오큰수, Time: 592 ms, Memory: 12260 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n], res[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        res[i] = -1;
+    }
+
+    stack<int> s;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!s.empty() && s.top() <= arr[i])
+            s.pop();
+        if (!s.empty())
+            res[i] = s.top();
+        s.push(arr[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << res[i] << ' ';
+}
