5a30351b5699952533f8c142fc85f6b15d3be3ab
starlike6617 <105418844+starlike6617@users.noreply.github.com>
2022-12-09 21:22:31 +0900

    [Silver III] Title: 문자열 집합, Time: 392 ms, Memory: 11848 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<string> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+
+    sort(v.begin(), v.end());
+
+    int cnt = 0;
+    while (m--)
+    {
+        string str;
+        cin >> str;
+
+        if (binary_search(v.begin(), v.end(), str))
+            cnt++;
+    }
+
+    cout << cnt;
+}
