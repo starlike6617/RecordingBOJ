c365ec414404a9acc245d2e08815226d497920f0
starlike6617 <105418844+starlike6617@users.noreply.github.com>
2022-12-11 04:13:27 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    int a[n][m], b[n][m];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> a[i][j];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> b[i][j];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+            cout << a[i][j] + b[i][j] << ' ';
+        cout << endl;
+    }
+}
