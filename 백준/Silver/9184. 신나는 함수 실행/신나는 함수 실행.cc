8ce958c6115197e09f251021d78c190280f700c3
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2022-12-15 00:17:42 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 432 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int dp[21][21][21];
+
+int w(int a, int b, int c)
+{
+    if (a <= 0 || b <= 0 || c <= 0)
+        return 1;
+    else if (a > 20 || b > 20 || c > 20)
+        return w(20, 20, 20);
+    else if (dp[a][b][c])
+        return dp[a][b][c];
+    else if (a < b && b < c)
+        return dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+    else
+        return dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+}
+
+int main()
+{
+    while (1)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+
+        if (x == -1 && y == -1 && z == -1)
+            break;
+        cout << "w(" << x << ", " << y << ", " << z << ") = "
+             << w(x, y, z) << '\n';
+    }
+}
