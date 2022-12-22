9b88aec5ab2ca2fbcf13c6d2d807ddffe0613e11
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2022-12-22 11:35:52 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n, k, sum;
+    cin >> n >> k;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        v[i] = i + 1;
+
+    cout << '<';
+    sum = k - 1;
+    while (v.size() != 1)
+    {
+        cout << v[sum] << ", ";
+        v.erase(v.begin() + sum);
+        sum = (sum + k - 1) % v.size();
+    }
+    cout << v.front() << '>';
+}
