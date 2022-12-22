cb373af3afc36187ef3d25fa09515c0f9e2dea9e
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2022-12-22 11:27:57 +0900

    [Gold IV] Title: 문자열 폭발, Time: 444 ms, Memory: 4916 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main()
+{
+    string str, bomb;
+    cin >> str >> bomb;
+
+    deque<char> d;
+    int cnt = 0;
+    for (char ch : str)
+    {
+        d.push_back(ch);
+
+        if (d.size() >= bomb.size())
+        {
+            string sub;
+            for (int i = 0; i < bomb.size(); i++)
+            {
+                string temp(1, d[d.size() - bomb.size() + i]);
+                sub += temp;
+            }
+
+            if (sub == bomb)
+                for (int i = 0; i < bomb.size(); i++)
+                    d.pop_back();
+        }
+    }
+
+    if (d.size())
+        for (char ch : d)
+            cout << ch;
+    else
+        cout << "FRULA";
+}
