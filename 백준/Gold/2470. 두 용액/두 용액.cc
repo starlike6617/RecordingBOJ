1f2244ca06a2506e22cc1b424fba9bb80f68bcc4
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-01-03 10:18:55 +0900

    [Gold V] Title: 두 용액, Time: 68 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    sort(arr, arr + n);
+
+    int start = 0, end = n - 1, min = 2000000000;
+    int ans1, ans2;
+    while (start < end)
+    {
+        int sum = arr[start] + arr[end];
+
+        if (min > abs(sum))
+        {
+            min = abs(sum);
+            ans1 = arr[start];
+            ans2 = arr[end];
+
+            if (sum == 0)
+                break;
+        }
+
+        if (sum < 0)
+            start++;
+        else
+            end--;
+    }
+
+    cout << ans1 << ' ' << ans2;
+}
