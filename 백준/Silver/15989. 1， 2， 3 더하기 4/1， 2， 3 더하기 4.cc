bc684a21d8f6b586d9862ba6c3f948c2c6bd0f68
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-01-16 01:15:56 +0900

    [Silver I] Title: 1, 2, 3 더하기 4, Time: 16 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Silver/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    int dp[10001][4];
+    dp[1][1] = 1;
+    dp[2][1] = 1, dp[2][2] = 1;
+    dp[3][1] = 1, dp[3][2] = 1, dp[3][3] = 1;
+
+    for (int i = 4; i < 10001; i++)
+    {
+        dp[i][1] = dp[i - 1][1];
+        dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+        dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+    }
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << dp[n][1] + dp[n][2] + dp[n][3] << '\n';
+    }
+}
