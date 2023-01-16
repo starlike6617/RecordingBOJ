3c4c31d9a36a0e457b88e092ed9c743c812900c8
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-01-17 00:11:40 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    int N;
+    cin >> N;
+
+    while (N--)
+    {
+        int x;
+        cin >> x;
+
+        if (x)
+            pq.push(x);
+        else
+        {
+            if (!pq.empty())
+            {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+            else
+                cout << 0 << '\n';
+        }
+    }
+}
