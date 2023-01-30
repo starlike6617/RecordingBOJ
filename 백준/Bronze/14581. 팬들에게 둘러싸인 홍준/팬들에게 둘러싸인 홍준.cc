7b48d882fae2fb3eb5ef2e6c6af3e8e2f7b04397
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-01-31 02:30:25 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    cout << ":fan::fan::fan:" << endl;
+    cout << ":fan::" << str << "::fan:" << endl;
+    cout << ":fan::fan::fan:";
+}
