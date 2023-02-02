e2118b2a41583a4a8f02f89a64de9da0b91f6205
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-02-03 07:52:47 +0900

    [Bronze V] Title: 三方比較 (Three-Way Comparison), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27328. 三方比較 （Three－Way Comparison）/三方比較 （Three－Way Comparison）.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+    if (A < B)
+        cout << -1;
+    else if (A == B)
+        cout << 0;
+    else
+        cout << 1;
+}
