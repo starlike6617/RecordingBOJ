41224064404d6a3a723eccc73d0f928c1a8ae99f
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-02-06 11:08:11 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    while (1)
+    {
+        int M, F;
+        cin >> M >> F;
+
+        if (M == 0 && F == 0)
+            break;
+
+        cout << M + F << endl;
+    }
+}
