8fde213aaa191a0855edf3fef5ffc977df32e3b3
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-02-07 18:05:31 +0900

    [Bronze IV] Title: Tabliczka, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8674. Tabliczka/Tabliczka.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long a, b;
+    cin >> a >> b;
+
+    if (a % 2 == 0 || b % 2 == 0)
+        cout << 0;
+    else
+        cout << min(a, b);
+}
