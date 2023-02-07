a8271f3e63215ead727b8d78e8f28472145104db
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-02-07 17:58:25 +0900

    [Bronze IV] Title: Triangles, Time: 408 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7595. Triangles/Triangles.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    while (1)
+    {
+        int n;
+        cin >> n;
+
+        if (n == 0)
+            break;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 0; j < i; j++)
+                cout << '*';
+            cout << endl;
+        }
+    }
+}
