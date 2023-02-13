5c05ade30cad272ec445cd7f280ea9f46e6da24a
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-02-13 23:33:20 +0900

    [Bronze IV] Title: Judging Moose, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15025. Judging Moose/Judging Moose.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int l, r;
+    cin >> l >> r;
+
+    if (l == 0 && r == 0)
+        cout << "Not a moose";
+    else if (l == r)
+        cout << "Even " << l + r;
+    else
+        cout << "Odd " << max(l, r) * 2;
+}
