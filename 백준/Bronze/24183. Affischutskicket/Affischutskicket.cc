2aa2a070b6eada26aedbdb25398e780c56e20bc8
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-02-21 18:50:52 +0900

    [Bronze IV] Title: Affischutskicket, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24183. Affischutskicket/Affischutskicket.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a, b, c;
+    cin >> a >> b >> c;
+    cout << (229 * 324 * 2 * a + 297 * 420 * 2 * b + 210 * 297 * c) / 1000000;
+}
