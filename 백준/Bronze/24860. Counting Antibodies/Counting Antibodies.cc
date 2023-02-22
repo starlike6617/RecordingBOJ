b13a53adfc72e2998d27e1b823f2ba202b7ab2ef
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-02-22 18:01:39 +0900

    [Bronze IV] Title: Counting Antibodies, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24860. Counting Antibodies/Counting Antibodies.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long vk, jk, vg, jg, vh, dh, jh;
+    cin >> vk >> jk >> vg >> jg >> vh >> dh >> jh;
+    cout << (vk * jk + vg * jg) * (vh * dh * jh);
+}
