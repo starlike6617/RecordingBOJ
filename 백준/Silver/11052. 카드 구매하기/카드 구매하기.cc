45fe0cef178b1d6ff21d6d6ab4a82e55e755537e
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-03-03 15:22:32 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    int P[N + 1], dp[N + 1];
+    for (int i = 1; i <= N; i++)
+    {
+        cin >> P[i];
+        dp[i] = P[i];
+    }
+
+    for (int i = 2; i <= N; i++)
+        for (int j = 1; j < i; j++)
+            dp[i] = max(dp[i], dp[i - j] + P[j]);
+
+    cout << dp[N];
+}
