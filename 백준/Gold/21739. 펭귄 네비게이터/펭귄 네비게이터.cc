fc18a50ef42d6f264fc2152ff12438e6d8607c78
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-03-23 11:00:16 +0900

    [Gold II] Title: 펭귄 네비게이터, Time: 92 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Gold/21739. 펭귄 네비게이터/펭귄 네비게이터.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#define mod 1000000007
+using namespace std;
+
+long long dp[10001];
+
+int main()
+{
+    dp[0] = 1, dp[1] = 1;
+    for (int i = 2; i <= 10000; i++)
+    {
+        for (int j = 0; j < i; j++)
+            dp[i] += (dp[j] * dp[i - j - 1]) % mod;
+        dp[i] %= mod;
+    }
+
+    int N;
+    cin >> N;
+    cout << dp[N];
+}
