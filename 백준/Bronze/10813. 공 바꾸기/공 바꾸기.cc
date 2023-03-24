64fcd8659efb91c510afb62648e0c383c9cb51ff
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-03-24 15:38:42 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+
+    int arr[N + 1];
+    for (int i = 1; i <= N; i++)
+        arr[i] = i;
+
+    while (M--)
+    {
+        int i, j;
+        cin >> i >> j;
+        swap(arr[i], arr[j]);
+    }
+
+    for (int i = 1; i <= N; i++)
+        cout << arr[i] << ' ';
+}
