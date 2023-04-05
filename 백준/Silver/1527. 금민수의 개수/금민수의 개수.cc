445c0ba8e90c3d58c61d48dc4e998138a958fb82
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-04-05 14:23:53 +0900

    [Silver I] Title: 금민수의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1527. 금민수의 개수/금민수의 개수.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int A, B;
+int cnt;
+
+void count(long long n)
+{
+    if (n > B)
+        return;
+
+    if (A <= n && n <= B)
+        cnt++;
+
+    count(n * 10 + 4);
+    count(n * 10 + 7);
+}
+
+int main()
+{
+    cin >> A >> B;
+
+    count(4);
+    count(7);
+
+    cout << cnt;
+}
