4e3418743debb8f5934500bb0f93ca893de95342
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-04-15 02:36:29 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 804 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    int arr[N];
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+
+    sort(arr, arr + N, greater<int>());
+
+    for (int i : arr)
+        cout << i << '\n';
+}
