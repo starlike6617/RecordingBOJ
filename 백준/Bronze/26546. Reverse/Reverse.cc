127d60ca10488addc11d5abff3167f87d6a89a2d
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-04-23 21:07:14 +0900

    [Bronze IV] Title: Reverse, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/26546. Reverse/Reverse.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        string str;
+        int i, j;
+        cin >> str >> i >> j;
+
+        for (int idx = 0; idx < str.size(); idx++)
+        {
+            if (i <= idx && idx < j)
+                continue;
+            cout << str[idx];
+        }
+        cout << endl;
+    }
+}
