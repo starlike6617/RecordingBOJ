0dcbb19c413ba91cd0b43cf7089890f14a92daa4
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-05-10 01:09:38 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -17,16 +17,17 @@ int main()
     for (int i = 1; i <= N; i++)
         cin >> c[i].T >> c[i].P;
 
-    int dp[N + 1] = {};
-    for (int i = 1; i <= N; i++)
+    int dp[N + 2] = {};
+    int maxDp = 0;
+
+    for (int i = N; i >= 1; i--)
     {
-        if (i + c[i].T - 1 <= N)
-        {
-            dp[i + c[i].T - 1] = max(dp[i + c[i].T - 1], dp[i - 1] + c[i].P);
-            for (int j = i + c[i].T; j <= N; j++)
-                dp[j] = max(dp[j], dp[i + c[i].T - 1]);
-        }
+        if (i + c[i].T - 1 > N)
+            dp[i] = dp[i + 1];
+        else
+            dp[i] = max(dp[i + 1], dp[i + c[i].T] + c[i].P);
+        maxDp = max(maxDp, dp[i]);
     }
 
-    cout << dp[N];
+    cout << maxDp;
 }
