6475d3dfc30db08fe14b6eb2227387ddc9dc5863
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-05-12 17:43:06 +0900

    [Gold I] Title: Farey Sequence Length, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11525. Farey Sequence Length/Farey Sequence Length.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int phi(int n)
+{
+    int res = n;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            res -= res / i;
+            while (n % i == 0)
+                n /= i;
+        }
+    }
+
+    if (n > 1)
+        res -= res / n;
+
+    return res;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int sum[10001];
+    sum[0] = 1;
+
+    for (int i = 1; i < 10001; i++)
+        sum[i] = sum[i - 1] + phi(i);
+
+    int P;
+    cin >> P;
+
+    while (P--)
+    {
+        int K, N;
+        cin >> K >> N;
+        cout << K << ' ' << sum[N] << '\n';
+    }
+}
