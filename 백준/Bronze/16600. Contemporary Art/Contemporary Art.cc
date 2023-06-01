49fb03626151430e570cbb0c51561a5720410414
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-06-01 09:32:47 +0900

    [Bronze IV] Title: Contemporary Art, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/16600. Contemporary Art/Contemporary Art.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    long long a;
+    cin >> a;
+
+    cout << fixed;
+    cout.precision(7);
+    cout << sqrt(a) * 4;
+}
