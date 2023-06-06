e2edbc58776bc31e810fa26c2e566154f8015a05
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-06-07 00:49:29 +0900

    [Bronze III] Title: 경고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3029. 경고/경고.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int h1, m1, s1, h2, m2, s2;
+    char colon;
+    cin >> h1 >> colon >> m1 >> colon >> s1;
+    cin >> h2 >> colon >> m2 >> colon >> s2;
+
+    int t1 = h1 * 3600 + m1 * 60 + s1;
+    int t2 = h2 * 3600 + m2 * 60 + s2;
+
+    int t = t2 - t1;
+    if (t < 0)
+        t += 24 * 3600;
+    else if (t == 0)
+        t = 24 * 3600;
+
+    int h = t / 3600;
+    int m = t % 3600 / 60;
+    int s = t % 60;
+
+    printf("%02d:%02d:%02d\n", h, m, s);
+}
