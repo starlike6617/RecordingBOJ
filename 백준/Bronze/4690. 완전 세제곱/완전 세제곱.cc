e3cead32c2fa55e81e2483a155cd26e4840ca690
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-06-09 23:57:16 +0900

    [Bronze III] Title: 완전 세제곱, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4690. 완전 세제곱/완전 세제곱.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int a = 2; a <= 100; a++)
+        for (int b = 2; b < a; b++)
+            for (int c = b; c < a; c++)
+                for (int d = c; d < a; d++)
+                    if (a * a * a == b * b * b + c * c * c + d * d * d)
+                        cout << "Cube = " << a << ", Triple = (" << b << ',' << c << ',' << d << ')' << endl;
+}
