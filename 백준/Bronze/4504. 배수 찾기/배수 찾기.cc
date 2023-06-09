7a9592f2500d214f7f1a539dc6e08c786ceec9c2
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-06-09 16:07:43 +0900

    [Bronze III] Title: 배수 찾기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4504. 배수 찾기/배수 찾기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    while (true)
+    {
+        int a;
+        cin >> a;
+
+        if (a == 0)
+            break;
+
+        if (a % n == 0)
+            cout << a << " is a multiple of " << n << "." << endl;
+        else
+            cout << a << " is NOT a multiple of " << n << "." << endl;
+    }
+}
