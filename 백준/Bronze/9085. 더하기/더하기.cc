9544d8dfad4b6f26f0b0be3d358a0f0002418786
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-06-27 10:52:54 +0900

    [Bronze III] Title: 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9085. 더하기/더하기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int N;
+        cin >> N;
+
+        int sum = 0;
+        while (N--)
+        {
+            int num;
+            cin >> num;
+            sum += num;
+        }
+
+        cout << sum << endl;
+    }
+}
