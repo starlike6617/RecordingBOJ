893dabcffd853885b884fee996b6631e2fcc2cfa
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-07-04 19:32:15 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅁ, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23806. 골뱅이 찍기 － ㅁ/골뱅이 찍기 － ㅁ.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N * 5; i++)
+    {
+        if (i < N || i >= N * 4)
+            for (int j = 0; j < N * 5; j++)
+                cout << '@';
+        else
+            for (int j = 0; j < N * 5; j++)
+            {
+                if (j < N || j >= N * 4)
+                    cout << '@';
+                else
+                    cout << ' ';
+            }
+        cout << endl;
+    }
+}
