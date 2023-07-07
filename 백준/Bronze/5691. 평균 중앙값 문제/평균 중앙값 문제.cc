dc9ec104764b1a17daa0e48e269704b2da14cf8b
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-07-07 21:40:09 +0900

    [Bronze III] Title: 평균 중앙값 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5691. 평균 중앙값 문제/평균 중앙값 문제.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    while (true)
+    {
+        int A, B;
+        cin >> A >> B;
+
+        if (A == 0 && B == 0)
+        {
+            break;
+        }
+
+        cout << A * 2 - B << endl;
+    }
+}
