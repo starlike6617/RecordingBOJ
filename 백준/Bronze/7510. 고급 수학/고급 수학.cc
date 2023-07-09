bba5451eb98e1202ec11ef906df937e450a0b300
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-07-09 23:45:27 +0900

    [Bronze III] Title: 고급 수학, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7510. 고급 수학/고급 수학.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int arr[3];
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> arr[i];
+        }
+
+        sort(arr, arr + 3);
+
+        cout << "Scenario #" << i << ":" << endl;
+        if (arr[0] * arr[0] + arr[1] * arr[1] == arr[2] * arr[2])
+        {
+            cout << "yes" << endl;
+        }
+        else
+        {
+            cout << "no" << endl;
+        }
+        cout << endl;
+    }
+}
