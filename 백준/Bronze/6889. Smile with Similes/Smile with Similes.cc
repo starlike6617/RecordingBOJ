9a47f8807cdca381e8b6c71feacb225b8fd355b1
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-07-11 03:58:40 +0900

    [Bronze III] Title: Smile with Similes, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/6889. Smile with Similes/Smile with Similes.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    string adj[n], noun[m];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> adj[i];
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cin >> noun[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << adj[i] << " as " << noun[j] << endl;
+        }
+    }
+}
