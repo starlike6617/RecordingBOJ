723d8cba3f200c518b7b54426d295fd6217b81b1
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-07-20 05:22:50 +0900

    [Bronze III] Title: 쿠폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10179. 쿠폰/쿠폰.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        double a;
+        cin >> a;
+
+        cout << fixed;
+        cout.precision(2);
+        cout << '$' << a * 0.8 << endl;
+    }
+}
