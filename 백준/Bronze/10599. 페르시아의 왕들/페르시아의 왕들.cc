8dd224f705eabcf8f6a0038d0ed2c50e196e5514
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-07-20 19:00:54 +0900

    [Bronze III] Title: 페르시아의 왕들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10599. 페르시아의 왕들/페르시아의 왕들.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    while (true)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        if (a == 0 && b == 0 & c == 0 & d == 0)
+        {
+            break;
+        }
+
+        cout << c - b << ' ' << d - a << endl;
+    }
+}
