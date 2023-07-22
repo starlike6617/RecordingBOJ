68ce0337cfd3a8dff50a287355fa8e669f3f072b
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-07-23 03:59:46 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N - 1 - i; j++)
+        {
+            cout << ' ';
+        }
+        cout << '*';
+
+        for (int j = 0; j < i; j++)
+        {
+            cout << " *";
+        }
+
+        cout << endl;
+    }
+}
