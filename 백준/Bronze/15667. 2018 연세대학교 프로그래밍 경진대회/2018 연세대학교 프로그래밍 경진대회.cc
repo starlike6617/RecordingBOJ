e5e73aa845933cdca73e745f599bd7c65d18bc1a
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-07-25 21:01:58 +0900

    [Bronze III] Title: 2018 연세대학교 프로그래밍 경진대회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15667. 2018 연세대학교 프로그래밍 경진대회/2018 연세대학교 프로그래밍 경진대회.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    for (int i = 1; i < N; i++)
+    {
+        if (1 + i + i * i == N)
+        {
+            cout << i;
+        }
+    }
+}
