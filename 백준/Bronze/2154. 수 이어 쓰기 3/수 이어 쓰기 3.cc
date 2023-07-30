b748c17862cfa851de295a306970707b4eb8ba40
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-07-31 04:51:11 +0900

    [Bronze II] Title: 수 이어 쓰기 3, Time: 4 ms, Memory: 2752 KB -BaekjoonHub

---
+++ 백준/Bronze/2154. 수 이어 쓰기 3/수 이어 쓰기 3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    string s = "";
+    for (int i = 1; i <= N; i++)
+    {
+        s += to_string(i);
+    }
+
+    cout << s.find(to_string(N)) + 1;
+}
