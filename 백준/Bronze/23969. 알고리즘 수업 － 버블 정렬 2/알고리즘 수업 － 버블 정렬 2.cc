63192c6eca2285a75658e2673d76205302f4133a
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-09-02 05:25:44 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 2, Time: 140 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23969. 알고리즘 수업 － 버블 정렬 2/알고리즘 수업 － 버블 정렬 2.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+
+    int arr[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int cnt = 0;
+    for (int i = N - 1; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                cnt++;
+            }
+
+            if (cnt == K)
+            {
+                for (int i : arr)
+                {
+                    cout << i << ' ';
+                }
+                return 0;
+            }
+        }
+    }
+
+    if (cnt < K)
+    {
+        cout << -1;
+    }
+}
