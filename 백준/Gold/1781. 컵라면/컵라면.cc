d2d39a49e1ac77334aaafa75b0823ef3f5043fea
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2023-09-06 15:59:34 +0900

    [Gold II] Title: 컵라면, Time: 84 ms, Memory: 6244 KB -BaekjoonHub

---
+++ 백준/Gold/1781. 컵라면/컵라면.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+using P = pair<int, int>;
+
+struct cmp
+{
+    bool operator()(P a, P b)
+    {
+        if (a.first == b.first)
+        {
+            return a.second < b.second;
+        }
+        return a.first > b.first;
+    }
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    priority_queue<P, vector<P>, cmp> pq;
+    while (N--)
+    {
+        int deadline, cup;
+        cin >> deadline >> cup;
+        pq.push({deadline, cup});
+    }
+
+    int cnt = 1;
+    priority_queue<int, vector<int>, greater<int>> resPq;
+
+    while (!pq.empty())
+    {
+        if (cnt <= pq.top().first)
+        {
+            resPq.push(pq.top().second);
+            cnt++;
+        }
+        else if (resPq.top() < pq.top().second)
+        {
+            resPq.pop();
+            resPq.push(pq.top().second);
+        }
+        pq.pop();
+    }
+
+    long long res = 0;
+    while (!resPq.empty())
+    {
+        res += resPq.top();
+        resPq.pop();
+    }
+    cout << res;
+}
