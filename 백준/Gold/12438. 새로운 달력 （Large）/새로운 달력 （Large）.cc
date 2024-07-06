75e756baa44f14deaa6211e9a1351aa4b21ab221
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2024-07-07 05:09:09 +0900

    [Gold V] Title: 새로운 달력 (Large), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12438. 새로운 달력 （Large）/새로운 달력 （Large）.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+long long gcd(long long a, long long b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    for (int i = 1; i <= T; i++)
+    {
+        long long m, d, w;
+        cin >> m >> d >> w;
+
+        long long cnt = m * d + w * (m - 1);
+        long long r = d % w;
+
+        if (r == 0)
+        {
+            cnt -= w * (m - 1);
+        }
+        else
+        {
+            long long k; // (r * k) % w == 0 (최솟값 k)
+            if (w % r == 0)
+                k = w / r;
+            else
+                k = w / gcd(w, r);
+
+            cnt -= w * (m / k);
+            if (m % k == 0)
+                cnt += w;
+        }
+
+        cout << "Case #" << i << ": ";
+        cout << (cnt + w - 1) / w << endl;
+    }
+}
