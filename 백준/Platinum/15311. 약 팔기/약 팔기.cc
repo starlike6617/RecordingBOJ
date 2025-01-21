998138268c651a5cb8f6d3e8c903a446a00614e4
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-01-21 16:55:07 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    cout << 2000 << endl;
+    for (int i = 0; i < 1000; i++)
+    {
+        cout << 1 << ' ';
+    }
+    for (int i = 0; i < 1000; i++)
+    {
+        cout << 1000 << ' ';
+    }
+}
