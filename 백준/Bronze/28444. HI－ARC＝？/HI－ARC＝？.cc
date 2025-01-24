a4be05f1a7b1873ebbf9e57317fae6435ed2ee70
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-01-24 17:40:59 +0900

    [Bronze V] Title: HI-ARC=?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+    vector<int> v(5);
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> v[i];
+    }
+    cout << v[0] * v[1] - v[2] * v[3] * v[4];
+}
