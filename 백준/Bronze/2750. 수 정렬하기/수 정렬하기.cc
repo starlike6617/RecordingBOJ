818ac48c8fb402c6ca9438001e1a6c2258b4f265
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-03-20 20:48:50 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[1000], n, temp;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++)
+		printf("%d\n", arr[i]);
+	return 0;
+}
