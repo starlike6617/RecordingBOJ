4b4a6248a13aa269b7dc6d045b725f006b07dafe
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-03-26 17:45:01 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.cc
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.cc
@@ -1,26 +1,12 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int N;
-    cin >> N;
-
-    int arr[N];
-    for (int i = 0; i < N; i++)
-        cin >> arr[i];
-
-    int set = 0, cnt = 0, res = 0;
-    for (int i = 1; i < N; i++)
-    {
-        if (arr[set] >= arr[i])
-            cnt++;
-        else
-        {
-            set = i;
-            cnt = 0;
-        }
-        res = max(res, cnt);
-    }
-    cout << res;
-}
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
