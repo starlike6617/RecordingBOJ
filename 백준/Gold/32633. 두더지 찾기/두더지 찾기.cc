90ca4a9ba612bedbc442d77ceb3a2b67208c9a1f
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-03-30 02:16:25 +0900

    [Gold V] Title: 두더지 찾기, Time: 24 ms, Memory: 4472 KB -BaekjoonHub

---
+++ 백준/Gold/32633. 두더지 찾기/두더지 찾기.cc
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define endl '\n'
+using namespace std;
+using ll = unsigned long long;
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b, ll L)
+{
+    ll g = gcd(a, b);
+    if (a / g > L / b)
+        return L + 1; // 오버플로우 방지를 위해 계산 도중 L보다 커지면 L+1을 반환
+    return (a / g) * b;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll N, L;
+    cin >> N >> L;
+
+    vector<ll> A(N), ones, zeros;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+    for (int i = 0; i < N; i++)
+    {
+        int check;
+        cin >> check;
+
+        if (check)
+            ones.push_back(A[i]);
+        else
+            zeros.push_back(A[i]);
+    }
+
+    if (!ones.empty())
+    {
+        sort(ones.begin(), ones.end(), greater<ll>());
+
+        ll res = 1;
+        for (int i = 0; i < ones.size(); i++)
+        {
+            res = lcm(res, ones[i], L);
+
+            if (res > L)
+            {
+                cout << -1;
+                return 0;
+            }
+        }
+
+        for (int i = 0; i < zeros.size(); i++)
+        {
+            if (res % zeros[i] == 0)
+            {
+                cout << -1;
+                return 0;
+            }
+        }
+
+        cout << res;
+    }
+    else
+    {
+        for (int i = 0; i < zeros.size(); i++)
+        {
+            if (zeros[i] == 1)
+            {
+                cout << -1;
+                return 0;
+            }
+        }
+        cout << 1;
+    }
+}
