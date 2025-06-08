c14b6a311311014fe77f0b546e1257e8d92b14c7
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-06-08 16:20:35 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main()
 {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    printf("%d", a+b);
-    return 0;
+    int A, B;
+    cin >> A >> B;
+    cout << A + B;
 }
