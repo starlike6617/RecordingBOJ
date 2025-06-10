5f3128b1982aa2348cad13a5d406023261d7eed9
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-06-10 13:09:11 +0900

    [Bronze IV] Title: 출석 이벤트, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25704. 출석 이벤트/출석 이벤트.cc
+++ 백준/Bronze/25704. 출석 이벤트/출석 이벤트.cc
@@ -3,22 +3,26 @@ using namespace std;
 
 int main()
 {
-    int N, P, res;
+    int N, P;
     cin >> N >> P;
 
+    int res = P;
+    if (N >= 5)
+    {
+        res = min(res, max(0, P - 500));
+    }
+    if (N >= 10)
+    {
+        res = min(res, max(0, P / 100 * 90));
+    }
+    if (N >= 15)
+    {
+        res = min(res, max(0, P - 2000));
+    }
     if (N >= 20)
-        res = min(P - 2000, P * 3 / 4);
-    else if (N >= 15)
-        res = min(P * 9 / 10, P - 2000);
-    else if (N >= 10)
-        res = min(P - 500, P * 9 / 10);
-    else if (N >= 5)
-        res = P - 500;
-    else
-        res = P;
+    {
+        res = min(res, max(0, P / 100 * 75));
+    }
 
-    if (res < 0)
-        cout << 0;
-    else
-        cout << res;
+    cout << res;
 }
