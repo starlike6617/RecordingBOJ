33c9831537f1d100b3125e551af7ada6b323d8a3
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-06-10 13:16:45 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    for (int i = 1; i <= T; i++)
+    {
+        int A, B;
+        cin >> A >> B;
+        cout << "Case #" << i << ": " << A + B << endl;
+    }
+}
