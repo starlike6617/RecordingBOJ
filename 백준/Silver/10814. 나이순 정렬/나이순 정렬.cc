af1f0de906095acee9bef7059bac24fd3a233604
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-06-14 15:00:16 +0900

    [Silver V] Title: 나이순 정렬, Time: 68 ms, Memory: 6720 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -1,31 +1,25 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#define endl '\n'
 using namespace std;
-
-class Person
-{
-public:
-    int age;
-    string name;
-};
-
-bool compare(Person p1, Person p2)
-{
-    return p1.age < p2.age;
-}
 
 int main()
 {
-    int n;
-    cin >> n;
+    int N;
+    cin >> N;
 
-    vector<Person> v(n);
-    for (int i = 0; i < n; i++)
-        cin >> v[i].age >> v[i].name;
+    vector<pair<int, pair<int, string>>> v(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> v[i].first >> v[i].second.second;
+        v[i].second.first = i;
+    }
 
-    stable_sort(v.begin(), v.end(), compare);
+    sort(v.begin(), v.end());
 
-    for (int i = 0; i < n; i++)
-        cout << v[i].age << ' ' << v[i].name << '\n';
+    for (auto it : v)
+    {
+        cout << it.first << ' ' << it.second.second << endl;
+    }
 }
