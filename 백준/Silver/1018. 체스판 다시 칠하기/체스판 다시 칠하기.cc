4aafcc727e6d335442049451f4a02a778aec47b3
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-06-16 23:28:58 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -3,43 +3,57 @@ using namespace std;
 
 int main()
 {
-    int n, m;
-    cin >> n >> m;
+    int N, M;
+    cin >> N >> M;
 
-    char wArr[8][8] = {
-        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
-        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
-        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
-        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
-        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
-        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
-        {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
-        {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}};
-
-    char arr[n][m];
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
-            cin >> arr[i][j];
-
-    int cnt = 0, res = 100;
-    for (int i = 0; i <= n - 8; i++)
+    char arr[50][50];
+    for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j <= m - 8; j++)
+        for (int j = 0; j < M; j++)
         {
-            cnt = 0;
-            for (int a = 0; a < 8; a++)
-            {
-                for (int b = 0; b < 8; b++)
-                {
-                    if (arr[i + a][j + b] != wArr[a][b])
-                        cnt++;
-                }
-            }
-            if (cnt > 32)
-                cnt = 64 - cnt;
-            if (cnt < res)
-                res = cnt;
+            cin >> arr[i][j];
         }
     }
+
+    int res = 65;
+    for (int a = 0; a < N - 7; a++)
+    {
+        for (int b = 0; b < M - 7; b++)
+        {
+            int WBcnt = 0, BWcnt = 0;
+
+            for (int i = a; i < a + 8; i++)
+            {
+                for (int j = b; j < b + 8; j++)
+                {
+                    if (((i - a) % 2) ^ ((j - b) % 2))
+                    {
+                        if (arr[i][j] == 'W')
+                        {
+                            WBcnt++;
+                        }
+                        else
+                        {
+                            BWcnt++;
+                        }
+                    }
+                    else
+                    {
+                        if (arr[i][j] == 'B')
+                        {
+                            WBcnt++;
+                        }
+                        else
+                        {
+                            BWcnt++;
+                        }
+                    }
+                }
+            }
+
+            res = min(res, min(WBcnt, BWcnt));
+        }
+    }
+
     cout << res;
 }
