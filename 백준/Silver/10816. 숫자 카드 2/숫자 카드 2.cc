fe2448518d2dd8dc1da2815f79dd31bad721890a
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-06-16 23:54:59 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 572 ms, Memory: 43828 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,26 +1,30 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
+#include <unordered_map>
 using namespace std;
 
 int main()
 {
-    int n, m;
-    cin >> n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    vector<int> v1(n);
-    for (int i = 0; i < n; i++)
-        cin >> v1[i];
-    cin >> m;
-    vector<int> v2(m);
-    for (int i = 0; i < m; i++)
-        cin >> v2[i];
+    int N;
+    cin >> N;
 
-    sort(v1.begin(), v1.end());
-    for (int i = 0; i < m; i++)
+    unordered_map<int, int> m;
+    while (N--)
     {
-        int upper = upper_bound(v1.begin(), v1.end(), v2[i]) - v1.begin();
-        int lower = lower_bound(v1.begin(), v1.end(), v2[i]) - v1.begin();
-        cout << upper - lower << ' ';
+        int num;
+        cin >> num;
+        m[num]++;
+    }
+
+    int M;
+    cin >> M;
+
+    while (M--)
+    {
+        int num;
+        cin >> num;
+        cout << m[num] << ' ';
     }
 }
