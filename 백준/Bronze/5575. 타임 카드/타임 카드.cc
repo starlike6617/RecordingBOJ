2f1f608031880cc32c688c6203aa5205f215d6f1
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-06-20 15:52:23 +0900

    [Bronze IV] Title: 타임 카드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5575. 타임 카드/타임 카드.cc
+++ 백준/Bronze/5575. 타임 카드/타임 카드.cc
@@ -1,13 +1,20 @@
 #include <iostream>
+#define endl '\n'
 using namespace std;
 
 int main()
 {
     for (int i = 0; i < 3; i++)
     {
-        int h1, m1, s1, h2, m2, s2, t;
+        int h1, m1, s1, h2, m2, s2;
         cin >> h1 >> m1 >> s1 >> h2 >> m2 >> s2;
-        t = h2 * 3600 + m2 * 60 + s2 - h1 * 3600 - m1 * 60 - s1;
-        cout << t / 3600 << ' ' << (t % 3600) / 60 << ' ' << t % 60 << endl;
+
+        int t = (h2 - h1) * 3600 + (m2 - m1) * 60 + (s2 - s1);
+
+        cout << t / 3600 << ' ';
+        t %= 3600;
+        cout << t / 60 << ' ';
+        t %= 60;
+        cout << t << endl;
     }
 }
