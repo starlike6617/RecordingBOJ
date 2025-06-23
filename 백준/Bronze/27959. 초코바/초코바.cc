2d3dc3a7f88ddd602119501a47da9269d7a31f44
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-06-23 11:24:24 +0900

    [Bronze V] Title: 초코바, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/27959. 초코바/초코바.cc
+++ 백준/Bronze/27959. 초코바/초코바.cc
@@ -1,10 +1,9 @@
 #include <iostream>
-#define endl '\n'
 using namespace std;
 
 int main()
 {
-	int N, M;
-	cin >> N >> M;
-	cout << (N * 100 >= M ? "Yes" : "No");
+    int N, M;
+    cin >> N >> M;
+    cout << (N * 100 >= M ? "Yes" : "No");
 }
