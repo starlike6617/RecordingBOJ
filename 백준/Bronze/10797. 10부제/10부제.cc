f96a43ed851ebab10917d12e8f000ab69bb31469
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-06-23 11:23:17 +0900

    [Bronze IV] Title: 10부제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10797. 10부제/10부제.cc
+++ 백준/Bronze/10797. 10부제/10부제.cc
@@ -3,16 +3,19 @@ using namespace std;
 
 int main()
 {
-    int n, arr[5];
+    int n;
     cin >> n;
-
+    
     int cnt = 0;
     for (int i = 0; i < 5; i++)
     {
-        cin >> arr[i];
-        if (arr[i] == n % 10)
+        int car;
+        cin >> car;
+        if (car == n) 
+        {
             cnt++;
+        }
     }
-
+    
     cout << cnt;
 }
