5eec5d7fdd65b116e4f831c790853e23fa7e6055
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-06-29 15:37:21 +0900

    [Bronze III] Title: 캥거루 세마리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.cc
+++ 백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.cc
@@ -3,8 +3,7 @@ using namespace std;
 
 int main()
 {
-    int arr[3];
-    for (int i = 0; i < 3; i++)
-        cin >> arr[i];
-    cout << max(arr[2] - arr[1] - 1, arr[1] - arr[0] - 1);
+    int A, B, C;
+    cin >> A >> B >> C;
+    cout << max(B - A - 1, C - B - 1);
 }
