7de15b3e65a0c2b9417c7bf000d1323cca8dbb5e
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-07-10 15:46:13 +0900

    [Silver IV] Title: 피하자, Time: 0 ms, Memory: 2020 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/25379. 피하자/피하자.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<int> v(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> v[i];
+        v[i] %= 2;
+    }
+
+    int evenCnt = 0, oddCnt = 0;
+    int evenLast = 0, oddLast = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        if (v[i] == 0)
+        {
+            evenCnt += i - evenLast;
+            evenLast++;
+        }
+        else
+        {
+            oddCnt += i - oddLast;
+            oddLast++;
+        }
+    }
+
+    cout << min(evenCnt, oddCnt);
+}
