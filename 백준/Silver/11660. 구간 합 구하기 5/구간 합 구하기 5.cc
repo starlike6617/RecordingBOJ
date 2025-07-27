2bd575da5b9b3b625b977f157a5171b52c65f2f8
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-07-27 14:50:37 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 112 ms, Memory: 6004 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,30 +1,29 @@
 #include <iostream>
+#define endl '\n'
 using namespace std;
-
-int arr[1025][1025];
-int dp[1025][1025];
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     int N, M;
     cin >> N >> M;
 
+    int arr[1025][1025] = {};
     for (int i = 1; i <= N; i++)
     {
         for (int j = 1; j <= N; j++)
         {
             cin >> arr[i][j];
-            dp[i][j] = arr[i][j] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
+            arr[i][j] = arr[i][j] + arr[i][j - 1] + arr[i - 1][j] - arr[i - 1][j - 1];
         }
     }
 
-    int x1, y1, x2, y2;
     while (M--)
     {
+        int x1, y1, x2, y2;
         cin >> x1 >> y1 >> x2 >> y2;
-        cout << dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1] << '\n';
+        cout << arr[x2][y2] - arr[x2][y1 - 1] - arr[x1 - 1][y2] + arr[x1 - 1][y1 - 1] << endl;
     }
 }
