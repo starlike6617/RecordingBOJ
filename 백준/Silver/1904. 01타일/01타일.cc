3dcc74007230104f06e587af0ebf39f7b52bd3fe
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-07-28 16:11:51 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.cc
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -1,18 +1,21 @@
 #include <iostream>
+#define MOD 15746
 using namespace std;
 
 int dp[1000001];
 
 int main()
 {
-    int n;
-    cin >> n;
+    int N;
+    cin >> N;
 
     dp[1] = 1;
     dp[2] = 2;
 
-    for (int i = 3; i <= n; i++)
-        dp[i] = dp[i - 1] % 15746 + dp[i - 2] % 15746;
+    for (int i = 3; i <= N; i++)
+    {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+    }
 
-    cout << dp[n] % 15746;
+    cout << dp[N];
 }
