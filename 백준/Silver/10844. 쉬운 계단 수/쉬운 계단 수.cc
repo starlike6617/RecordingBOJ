e3acf03c7bb4d4e0a18930f1a4a242c641590fc6
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-07-29 12:24:11 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -1,34 +1,39 @@
 #include <iostream>
-#define mod 1000000000
+#include <vector>
+#include <algorithm>
+#define endl '\n'
+#define MOD 1000000000
 using namespace std;
-
-long long dp[101][10];
-long long res;
 
 int main()
 {
     int N;
     cin >> N;
 
+    int dp[101][10] = {};
+
     dp[1][0] = 0;
     for (int i = 1; i <= 9; i++)
+    {
         dp[1][i] = 1;
+    }
 
     for (int i = 2; i <= N; i++)
-        for (int j = 0; j <= 9; j++)
-        {
-            if (j == 0)
-                dp[i][j] += dp[i - 1][j + 1] % mod;
-            else if (j == 9)
-                dp[i][j] += dp[i - 1][j - 1] % mod;
-            else
-            {
-                dp[i][j] += dp[i - 1][j + 1] % mod;
-                dp[i][j] += dp[i - 1][j - 1] % mod;
-            }
-        }
+    {
+        dp[i][0] = dp[i - 1][1] % MOD;
+        dp[i][9] = dp[i - 1][8] % MOD;
 
+        for (int j = 1; j <= 8; j++)
+        {
+            dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+        }
+    }
+
+    int res = 0;
     for (int i = 0; i <= 9; i++)
-        res += dp[N][i] % mod;
-    cout << res % mod;
+    {
+        res = (res + dp[N][i]) % MOD;
+    }
+
+    cout << res;
 }
