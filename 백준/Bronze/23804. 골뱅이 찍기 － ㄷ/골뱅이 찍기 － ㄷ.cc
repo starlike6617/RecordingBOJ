8f7b4f1201cf65579a34eccc9e4d0de97f635326
Kitae Kwon <105418844+starlike6617@users.noreply.github.com>
2025-08-13 23:38:42 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄷ, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.cc
+++ 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.cc
@@ -6,14 +6,30 @@ int main()
     int N;
     cin >> N;
 
-    for (int i = 0; i < N * 5; i++)
+    for (int i = 0; i < N; i++)
     {
-        if (i < N || i >= N * 4)
-            for (int j = 0; j < N * 5; j++)
-                cout << '@';
-        else
-            for (int j = 0; j < N; j++)
-                cout << '@';
+        for (int j = 0; j < 5 * N; j++)
+        {
+            cout << '@';
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < 3 * N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << '@';
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < 5 * N; j++)
+        {
+            cout << '@';
+        }
         cout << endl;
     }
 }
